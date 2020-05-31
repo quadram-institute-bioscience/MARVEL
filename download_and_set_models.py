d5c6f1921df612b0e085d7cb0263db221f665ea0
Ubuntu <ubuntu@gmh.climb>
2020-05-31 17:53:13 +0000

    v 0.2 rc

--- download_and_set_models.py
+++ download_and_set_models.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # coding: utf-8
 #
 #
@@ -8,30 +8,70 @@
 
 # Libraries
 import os
+import re
+import sys
 import subprocess
 
 
+# Print to STDERR [proch]
+def print_msg(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
-# Greeting message
-print('\nYou only need to run this script once!\n')
+def fix_path(dir):
+    if not re.search('/$', dir):
+        return dir + '/'
+    else:
+        return dir
+
+print_msg("Usage: [Dest_Dir] [Tmp_Dir]")
+
+if len(sys.argv) >= 2:
+    db_dir = fix_path(sys.argv[1])
+else:
+    db_dir = fix_path(script_dir + '/models/')
+
+
+if len(sys.argv) == 3:
+    tmp_dir = fix_path(sys.argv[2] + '/')
+else:
+    tmp_dir = fix_path('/tmp/')
+
+script_dir = os.getcwd()
+repository = 'https://github.com/quadram-institute-bioscience/MARVEL/raw/master/models/'
+print_msg("Destination directory: {}\nTemp directory: {}".format(db_dir, tmp_dir) )
+
+models = [ 'pickle_model_rfc_trained_bins8k_refseq_all_3features_den_stran_prophitshmm.pkl', 
+ 'pickle_model_rfc_trained_bins_refseq_until2015_3features_den_stran_prophitshmm.pkl',
+ 'pickle_model_rfc_trained_bins_refseq_until2015_4features_wt_outliers_even2.pkl']
 
 # Verify databases
-if not os.path.isfile('models/all_vogs_hmm_profiles_feb2018.hmm.h3m'):
-    print('Downloading flat file database. Do not worry, that will just take a few minutes and is executed only in the first time... \n')
-    os.system('wget http://projetos.lbi.iq.usp.br/metazoo/deyvid/datasets/AllvogHMMprofiles.tar.gz')
-    print('Extracting database file...\n')
-    if subprocess.call('tar -xzf AllvogHMMprofiles.tar.gz', shell=True) == 1:
-        print('Error extracting database\n')
+if not os.path.isfile(db_dir + 'all_vogs_hmm_profiles_feb2018.hmm.h3m') or not os.path.isfile(db_dir + models[0]):
+    print_msg('[1] Downloading flat file database...')
+    os.system('wget --quiet -O "{}" http://projetos.lbi.iq.usp.br/metazoo/deyvid/datasets/AllvogHMMprofiles.tar.gz'.format(tmp_dir + 'AllvogHMMprofiles.tar.gz') )
+    
+    print_msg('[2] Extracting database file...')
+    extract_cmd = 'tar -xzf "{}"  -C "{}"'.format(tmp_dir + 'AllvogHMMprofiles.tar.gz', tmp_dir)
+   
+    if subprocess.call(extract_cmd, shell=True) != 0:
+        print('FATAL ERROR: Unable to extract database\n')
         quit()
-    subprocess.run('cat AllvogHMMprofiles/* > models/all_vogs_hmm_profiles_feb2018.hmm', shell=True)
-    subprocess.run('rm -r AllvogHMMprofiles/ AllvogHMMprofiles.tar.gz', shell=True)
-    print('Compressing hmm database...')
-    if subprocess.call('hmmpress models/all_vogs_hmm_profiles_feb2018.hmm', shell=True) == 1:
-        print('Error using hmmer tools (hmmpress). Verify if it is installed!\n')
+
+    subprocess.run('cat "{}"/* > "{}"/all_vogs_hmm_profiles_feb2018.hmm'.format(tmp_dir + 'AllvogHMMprofiles', db_dir), shell=True)
+
+    print_msg('[3] Downloading models')
+    for file in models:
+        print_msg(' - {}'.format(file))
+        wget_cmd = 'wget --quiet -O "{}/{}" "{}{}"'.format(db_dir, file, repository, file)
+        os.system( wget_cmd )
+    
+    print_msg('[4] Compressing hmm database...')
+    if subprocess.call('hmmpress "{}"/all_vogs_hmm_profiles_feb2018.hmm'.format(db_dir), shell=True) != 0:
+        print_msg('Error using hmmer tools (hmmpress). Verify if it is installed!\n')
+        print_msg('hmmpress "{}"/all_vogs_hmm_profiles_feb2018.hmm'.format(db_dir))
         quit()
-    print('Database is all set!\n')
+
+    subprocess.run('rm -r "{}"/AllvogHMMprofiles/ "{}"/AllvogHMMprofiles.tar.gz'.format(tmp_dir, tmp_dir), shell=True)
+
+    print_msg('Done.')
 else:
-    print('HMM Database is already set.\n')
-
-
-print('Thank you for using MARVEL.')
+    print('INFO: HMM Database found.\n')
