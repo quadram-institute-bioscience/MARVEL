9fec79fa6a0a2d0b4e21ff19b4736c33b79616e7
Andrea Telatin <andrea@telatin.com>
2020-06-01 10:51:56 +0000

    Updated test; Update Marvel

--- test/all.sh
+++ test/all.sh
@@ -21,8 +21,16 @@ mkdir -p test_prokka
 perl "$DIR"/../marvel_prokka --kingdom Viruses --centre X --compliant --gcode 11 --force --quiet --fast --norrna --notrna --cdsrnaolap --noanno --cpus 2 --prefix test \
  --outdir test_prokka/custom_test "$DATA_DIR"/Negative_CP011125.fasta
 
+rm -rf test_prokka
 
 echo ' *** TEST MARVEL '
 ./marvel -i "$DATA_DIR" -o test_output -t 2 -f  --debug --verbose
+
+if [[ $(ls test_output/phage_bins/ | grep Negative  |wc -l) -eq 0 ]]; then
+  echo "OK: No negative control found"
+fi
+if [[ $(ls test_output/phage_bins/ | grep Positive  |wc -l) -gt 0 ]]; then
+  echo "OK: Positive control found"
+fi
 
 rm -rf test_output
