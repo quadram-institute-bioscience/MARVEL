f20f68fdf4f854214e3e09d304ac8697ba94ae95
Andrea Telatin <andrea@telatin.com>
2020-06-01 11:10:46 +0100

    Update all.sh

--- test/all.sh
+++ test/all.sh
@@ -4,24 +4,25 @@ DATA_DIR="$DIR/../example_data/1/"
 set -euxo pipefail
 chmod +x marvel  download_and_set_models.py
 
-echo 'CHECK HMMPRESS'
+echo ' *** CHECK HMMPRESS'
 hmmpress -h
 
-echo 'DOWNLOAD DATABASE'
+echo ' *** CHECK INPUT'
+ls -l "$DATA_DIR"
+
+echo ' *** DOWNLOAD DATABASE'
 
 ./download_and_set_models.py
 
-echo 'TEST PROKKA'
+echo ' *** TEST CUSTOM PROKKA'
 
-prokka --kingdom Viruses --centre X --compliant --gcode 11 --force --quiet --fast --norrna --notrna --cdsrnaolap --noanno --cpus 2 --prefix test --outdir test_prokka/custom_test "$DATA_DIR"/Negative_CP011125.fasta
+mkdir -p test_prokka
 
-echo 'TEST CUSTOM PROKKA'
-
-perl "$DIR"/../marvel_prokka --kingdom Viruses --centre X --compliant --gcode 11 --force --quiet --fast --norrna --notrna --cdsrnaolap --noanno --cpus 2 --prefix test --outdir test_prokka/custom_test "$DATA_DIR"/Negative_CP011125.fasta
-
+perl "$DIR"/../marvel_prokka --kingdom Viruses --centre X --compliant --gcode 11 --force --quiet --fast --norrna --notrna --cdsrnaolap --noanno --cpus 2 --prefix test \
+ --outdir test_prokka/custom_test "$DATA_DIR"/Negative_CP011125.fasta
 
 
-
+echo ' *** TEST MARVEL '
 ./marvel -i "$DATA_DIR" -o test_output -t 2 -f  --debug --verbose
 
 rm -rf test_output
