b9f09679200f2e74a131db0b443ac4894d876c7a
Andrea Telatin <andrea@telatin.com>
2020-06-01 10:47:11 +0100

    Update all.sh

--- test/all.sh
+++ test/all.sh
@@ -2,7 +2,25 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 DATA_DIR="$DIR/../example_data/1/"
 set -euxo pipefail
-chmod +x marvel
+chmod +x marvel  download_and_set_models.py
+
+echo 'CHECK HMMPRESS'
+hmmpress -h
+
+echo 'DOWNLOAD DATABASE'
+
+./download_and_set_models.py
+
+echo 'TEST PROKKA'
+
+prokka --kingdom Viruses --centre X --compliant --gcode 11 --force --quiet --fast --norrna --notrna --cdsrnaolap --noanno --cpus 2 --prefix test --outdir test_prokka/custom_test "$DATA_DIR"/Negative_CP011125.fasta
+
+echo 'TEST CUSTOM PROKKA'
+
+perl "$DIR"/../marvel_prokka --kingdom Viruses --centre X --compliant --gcode 11 --force --quiet --fast --norrna --notrna --cdsrnaolap --noanno --cpus 2 --prefix test --outdir test_prokka/custom_test "$DATA_DIR"/Negative_CP011125.fasta
+
+
+
 
 ./marvel -i "$DATA_DIR" -o test_output -t 2 -f  --debug --verbose
 
